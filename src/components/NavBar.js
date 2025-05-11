3313f0d032d9ee535cc69e155ead3afe4c942689
Dinesh Rathnasiri <dineshrathnasiri017@gmail.com>
2025-05-11 22:23:26 +0530

    update

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -28,7 +28,7 @@ export default function NavBar() {
   return (
     <>
       {/* Mobile Menu Toggle */}
-      <div className="md:hidden fixed top-4 left-4 z-50 sm:hidden">
+      <div className="md:hidden fixed top-4 left-4 z-50 ">
         <button
           onClick={() => setOpen(!open)}
           className="bg-primary p-2 rounded-md text-white"
@@ -42,7 +42,7 @@ export default function NavBar() {
       </div>
 
       {/* Sidebar */}
-      <aside
+      <div
         className={`fixed top-0 left-0 h-full w-48 bg-white border-r shadow-lg z-40 transform ${
           open ? "translate-x-0" : "-translate-x-full"
         } md:translate-x-0 transition-transform duration-300 ease-in-out`}
@@ -71,7 +71,7 @@ export default function NavBar() {
             </Link>
           ))}
         </nav>
-      </aside>
+      </div>
     </>
   );
 }
